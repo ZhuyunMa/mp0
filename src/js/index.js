8b72276c83d5efaa0416239eaac55edab961f164
ZhuyunMa <zhuyunm2@illinois.edu>
2025-09-08 18:25:08 -0500

    update css and html

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../css/main.css';
 console.log('🔥 ZhuyunMA', new Date().toISOString());
+console.log("✅ Loaded src/js/index.js");
 
 function changeBg(color) {
     document.body.style.backgroundColor = color;
